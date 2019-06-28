4a2fab6f021bfdb778084d47731046f423fc0f8a
hardeepkaur <hardeepwalia019@gmail.com>
2019-06-27 21:10:14 -0400

    Setup session phone side

--- Activity2/ViewController.swift
+++ Activity2/ViewController.swift
@@ -7,8 +7,21 @@
 //
 
 import UIKit
+import WatchConnectivity
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WCSessionDelegate {
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        <#code#>
+    }
+    
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        <#code#>
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        <#code#>
+    }
+    
     
     @IBOutlet weak var PhoneOutputLabel: UILabel!
     
@@ -16,13 +29,30 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if (WCSession.isSupported()) {
+            print("PHONE: Phone supports WatchConnectivity!")
+            let session = WCSession.default
+            session.delegate = self
+            session.activate()
+        }
+            
+            else {
+                print("PHONE: Phone does not support WatchConnectivity")
+                
+
     }
     
-    @IBAction func ButtonPressed(_ sender: Any) {
+        func ButtonPressed(_ sender: Any) {
         
-        print("+HELLO! YOU PUSHED THE BUTTON!")
-        // output to label
-        self.PhoneOutputLabel.text = "Button pressed!"
+        if (WCSession.default.isReachable) {
+            // construct the message you want to send
+            // the message is in dictionary
+            let message = ["Message": "Hello"]
+            // send the message to the watch
+            WCSession.default.sendMessage(message, replyHandler: nil)
+        }
+
+    }
     }
     
 
